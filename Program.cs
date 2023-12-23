df6e6340b42d05409099841dc39183d34e1e60f3
João Carvalho <joaoguilhermecn@gmail.com>
2023-12-22 23:48:10 -0300

    Projeto #2 Concluido
    Segundo desafio do DecolaTech feito!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone iphone = new Iphone(numero:"111111",modelo:"pro",imei:"12121", memoria:10);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("zap");
+
+Smartphone nokia = new Nokia(numero:"111111",modelo:"pro",imei:"12121", memoria:10);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("zap");
